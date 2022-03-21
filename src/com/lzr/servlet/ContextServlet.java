3c23a5887207075dedb79225fac3ba9de493aadd
65137 <cnalzr@gmail.com>
2022-03-21 15:13:50 +0800

    2022年3月21日 Mybatis复习

--- src/com/lzr/servlet/ContextServlet.java
+++ src/com/lzr/servlet/ContextServlet.java
@@ -12,6 +12,11 @@ public class ContextServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        1. 获取web.xml中配置的上下文参数 context-param
+//        ContextServlet contextServlet = new ContextServlet();
+//        System.out.println(contextServlet);
+//        2. 获取当前的工程路径，格式：/工程路径
+//        3. 获取工程部署后在服务器硬盘上的绝对路径
 
     }
 }
